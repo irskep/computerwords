7b18c117582cc243fcbdb897d8d25b59f1eb1af7
Steve Johnson <sjohnson@hipmunk.com>
2016-02-24 17:04:09 -0800

    Allow specifying 'universal' processors

--- computerwords/library.py
+++ computerwords/library.py
@@ -6,13 +6,20 @@ class Library:
     def __init__(self):
         super().__init__()
         self.tag_name_to_processors = {}
+        self.universal_processors = []
 
     def _set_processor(self, tag_name, p, before_others=False):
-        self.tag_name_to_processors.setdefault(tag_name, [])
-        if before_others:
-            self.tag_name_to_processors[tag_name].insert(0, p)
+        if tag_name == '*':
+            if before_others:
+                self.universal_processors.insert(0, p)
+            else:
+                self.universal_processors.append(p)
         else:
-            self.tag_name_to_processors[tag_name].append(p)
+            self.tag_name_to_processors.setdefault(tag_name, [])
+            if before_others:
+                self.tag_name_to_processors[tag_name].insert(0, p)
+            else:
+                self.tag_name_to_processors[tag_name].append(p)
 
     # this is a decorator or a normal function
     def processor(self, tag_name, p=None, before_others=False):
@@ -26,16 +33,17 @@ class Library:
             return p
 
     def get_processors(self, tag_name, strict=True):
+        yield from self.universal_processors
         if strict:
             try:
-                return self.tag_name_to_processors[tag_name]
+                yield from self.tag_name_to_processors[tag_name]
             except KeyError:
                 msg = (
                     "No processors are defined for nodes with name {!r}."
                 ).format(tag_name)
                 raise UnknownNodeNameError(msg)
         else:
-            return self.tag_name_to_processors.get(tag_name, [])
+            yield from self.tag_name_to_processors.get(tag_name, [])
 
     def get_allowed_tags(self):
         return set(self.tag_name_to_processors.keys())
