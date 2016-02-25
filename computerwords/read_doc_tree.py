689e4ea4ac6efafe9b827bbaa142c8dc36b832e5
Steve Johnson <steve@graffitilabs.com>
2016-02-25 11:09:29 -0800

    Handle multi-glob case

--- computerwords/read_doc_tree.py
+++ computerwords/read_doc_tree.py
@@ -10,6 +10,10 @@ class DocTreeError(Exception): pass
 DocSubtree = namedtuple('DocSubtree', ['root_path', 'document_id', 'children'])
 
 
+def chain_list(lists):
+    return list(chain(*lists))
+
+
 class DocTree:
     def __init__(self, entries):
         super().__init__()
@@ -28,10 +32,8 @@ class DocTree:
 def _glob_or_set_of_globs_to_doc_hierarchy_entry(files_root, entry):
     if isinstance(entry, str):
         globs = sorted(files_root.glob(entry))
-        if len(globs) != 1:
-            raise ValueError("Handle this case please")
-        return DocSubtree(
-            globs[0], str(globs[0].relative_to(files_root)), [])
+        for path in sorted(globs):
+            yield DocSubtree(path, str(path.relative_to(files_root)), [])
     elif isinstance(entry, dict):
         if len(entry) != 1:
             raise DocTreeError("Only one key per dict allowed")
@@ -44,10 +46,10 @@ def _glob_or_set_of_globs_to_doc_hierarchy_entry(files_root, entry):
         if not isinstance(sub_entries, list):
             raise DocTreeError("Subtree values must be either string or list")
 
-        return DocSubtree(
+        yield DocSubtree(
             path, doc_id,
-            [_glob_or_set_of_globs_to_doc_hierarchy_entry(
-                files_root, sub_entry) for sub_entry in sub_entries])
+            chain_list([_glob_or_set_of_globs_to_doc_hierarchy_entry(
+                files_root, sub_entry) for sub_entry in sub_entries]))
     else:
         raise ValueError("Unsupported file_hierarchy value: {}".format(entry))
 
@@ -65,12 +67,11 @@ def doc_subtree_to_cwdom(subtree, get_doc_cwdom):
         yield from doc_subtree_to_cwdom(child, get_doc_cwdom)
 
 
-
 def read_doc_tree(root_path, file_hierarchy_conf, get_doc_cwdom):
-    doc_tree = DocTree([
+    doc_tree = DocTree(chain_list([
         _glob_or_set_of_globs_to_doc_hierarchy_entry(root_path, entry)
         for entry in file_hierarchy_conf
-    ])
-    document_nodes = list(
-        chain(*[doc_subtree_to_cwdom(doc, get_doc_cwdom) for doc in doc_tree]))
+    ]))
+    document_nodes = chain_list(
+        [doc_subtree_to_cwdom(doc, get_doc_cwdom) for doc in doc_tree])
     return doc_tree, document_nodes
