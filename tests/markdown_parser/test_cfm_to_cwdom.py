547e6c8d888b1c32741696431e76ef2e466cb0f0
Steve Johnson <sjohnson@hipmunk.com>
2016-02-29 18:05:34 -0800

    Add failing HTML parser tests

--- tests/markdown_parser/test_cfm_to_cwdom.py
+++ tests/markdown_parser/test_cfm_to_cwdom.py
@@ -1,10 +1,12 @@
+import unittest
+
 from computerwords.cwdom.CWDOMNode import *
 from computerwords.markdown_parser.cfm_to_cwdom import cfm_to_cwdom
 
 from tests.CWTestCase import CWTestCase
 
 
-TAGS = {'a', 'b', 'c'}
+TAGS = {'a', 'b', 'c', 'x', 'y', 'z'}
 
 
 class IntegratedHTMLParsingTestCase(CWTestCase):
@@ -19,3 +21,74 @@ class IntegratedHTMLParsingTestCase(CWTestCase):
                   ' c'
             """)
         )
+
+    @unittest.skip("")
+    def test_block_html_1(self):
+        s = self.strip("""
+            <x>some test
+            </x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
+
+    @unittest.skip("")
+    def test_block_html_2(self):
+        s = self.strip("""
+            <x>some test
+
+            </x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
+
+    @unittest.skip("")
+    def test_block_html_3(self):
+        s = self.strip("""
+            <x>
+
+            some test</x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
+
+    @unittest.skip("")
+    def test_block_html_4(self):
+        s = self.strip("""
+            <x>
+            some test
+            </x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
+
+    @unittest.skip("")
+    def test_block_html_5(self):
+        s = self.strip("""
+            <x>
+            some test
+
+            </x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
+
+    @unittest.skip("")
+    def test_block_html_6(self):
+        s = self.strip("""
+            <x>
+
+            some test
+            </x>
+        """)
+
+    @unittest.skip("")
+    def test_block_html_7(self):
+        s = self.strip("""
+            <x>
+
+            some test
+
+            </x>
+        """)
+        root = CWDOMRootNode(cfm_to_cwdom(s, TAGS))
+        self.log_node(root)
