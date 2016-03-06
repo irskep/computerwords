b157dc6a38ee73e0c3eff427a1becb9573f8b673
Steve Johnson <steve@graffitilabs.com>
2016-03-05 20:05:07 -0800

    Try to get pip deps working

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from setuptools import setup, find_packages
 
+reqs = [
+    "CommonMark (>=0.6, <0.7)",
+    "Pygments (>=2.1, <3.0)",
+]
+
 setup(
     name='Computer Words',
     version='0.5',
@@ -11,8 +16,6 @@ setup(
     url='https://computerwords.net/',
     packages=find_packages(exclude=['tests.*']),
     include_package_data=True,
-    requires=[
-        "CommonMark (>=0.6, <0.7)",
-        "Pygments (>=2.1, <3.0)",
-    ],
+    requires=reqs,
+    install_requires=reqs,
 )
