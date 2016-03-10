74bf6e7bcc9368763d7289c53c4bb939f88b09b0
Steve Johnson <steve@graffitilabs.com>
2016-03-09 19:56:52 -0800

    Autodoc can walk the symbol tree

--- computerwords/stdlib/src_py.py
+++ computerwords/stdlib/src_py.py
@@ -1,7 +1,12 @@
 import json
 import pathlib
+from collections import namedtuple
 
 from computerwords.cwdom.nodes import CWTagNode, CWTextNode
+
+
+SymbolDef = namedtuple(
+    'SymbolDef', ['id', 'parent_id', 'type', 'name', 'docstring', 'children'])
 
 
 def read_config(config):
@@ -10,13 +15,56 @@ def read_config(config):
         config['root_dir'].joinpath(symbols_path))
 
 
+def _create_symbol_tree(symbol_defs):
+    # {"parent": 1, "docstring": null, "id": 2, "name": "__main__", "type": "module"}
+    nodes_by_id = {}
+    for symbol in symbol_defs:
+        nodes_by_id[symbol['id']] = SymbolDef(children=[], **symbol)
+
+    roots = []
+    for symbol in nodes_by_id.values():
+        if symbol.parent_id:
+            nodes_by_id[symbol.parent_id].children.append(symbol)
+        else:
+            roots.append(symbol)
+    assert(len(roots) == 1)
+    return roots[0]
+
+
+def _debug_print_tree(t, i=0):
+    print("{}SymbolDef({}, {}, {})".format(" " * i, t.id, t.type, t.name))
+    for child in t.children:
+        _debug_print_tree(child, i + 2)
+
+
+def _get_symbol_at_path(t, parts):
+    print(parts)
+    for child in t.children:
+        print(child.name)
+    next_symbol = [s for s in t.children if s.name == parts[0]][0]
+    if len(parts) == 1:
+        return next_symbol
+    else:
+        return _get_symbol_at_path(next_symbol, parts[1:])
+
+
+
+def get_symbol_at_path(root, path):
+    parts = path.split('.')
+    assert(parts[0] == root.name)
+    return _get_symbol_at_path(root, parts[1:])
+
+
 def add_src_py(library):
     @library.processor('autodoc-python')
     def process_autodoc_module(tree, node):
         if 'autodoc_symbols' not in tree.processor_data:
             config = tree.env['config']
             with config['python']['resolved_symbols_path'].open() as f:
-                tree.processor_data['autodoc_symbols'] = [
-                    json.loads(line) for line in f]
-            print(tree.processor_data['autodoc_symbols'])
+                symbol_defs = [json.loads(line) for line in f]
+                tree.processor_data['autodoc_symbols'] = symbol_defs
+                symbol_tree = _create_symbol_tree(symbol_defs)
+                tree.processor_data['autodoc_symbol_tree'] = symbol_tree
+
+        symbol_tree = tree.processor_data['autodoc_symbol_tree']
         tree.replace_subtree(node, CWTextNode("YOU DID A THING"))
