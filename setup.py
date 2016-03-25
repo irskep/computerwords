8b72ab1895e6a9b39e4a33efea3ed21cbe2ba511
Steve Johnson <sjohnson@hipmunk.com>
2016-03-25 11:01:15 -0700

    Beefier setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,24 @@ from setuptools import setup, find_packages
 VERSION = '1.0a1'
 
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='computerwords',
     version=VERSION,
+
     description='A hopefully pretty good documentation system',
+    long_description=readme(),
+    license='BSD 3-clause',
+
     author='Steve Johnson',
     author_email='steve@steveasleep.com',
+
     url='https://steveasleep.com/computerwords/',
+
     packages=find_packages(exclude=['tests.*']),
     include_package_data=True,
     requires=[
@@ -26,4 +37,15 @@ setup(
 
     download_url='https://github.com/irskep/computerwords/tarball/' + VERSION,
     keywords=['documentation', 'docs', 'markdown'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing :: Markup',
+    ],
 )
