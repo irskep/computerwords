4a790830bd4f83433e240621c43b37dc428677b4
Steve Johnson <steve@graffitilabs.com>
2016-05-08 14:35:35 -0700

    Declare pathlib dependency for python versions <3.4

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup, find_packages
 
 
-VERSION = '1.0a8'
+VERSION = '1.0b1'
 
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+
+needs_pathlib = sys.version_info < (3, 4)
+legacy_requirements = ["pathlib>=1.0"] if needs_pathlib else []
 
 
 setup(
@@ -26,23 +31,19 @@ setup(
 
     packages=find_packages(exclude=['tests.*']),
     include_package_data=True,
-    requires=[
-        "CommonMark (>=0.6, <0.7)",
-        "Pygments (>=2.1, <3.0)",
-    ],
     install_requires=[
         "CommonMark>=0.6,<0.7",
         "Pygments>=2.1,<3.0",
-    ],
+    ] + legacy_requirements,
 
     download_url='https://github.com/irskep/computerwords/tarball/' + VERSION,
     keywords=['documentation', 'docs', 'markdown'],
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
